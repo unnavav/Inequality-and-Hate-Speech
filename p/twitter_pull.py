35e0475e80357419799c8bbc702026a64dfa13f4
unnavav <unnava.v@gmail.com>
2021-07-05 15:21:16 -0700

    updated to query for any word in a list of queries, still manually updating file names

--- p/twitter_pull.py
+++ p/twitter_pull.py
@@ -4,7 +4,8 @@
 
 # This file takes the bearer token provided by twitter's academic track and uses it to query the full-archive search to pull a random sampling of tweets from times throughout a given time period. The tweet data includes time sent, tweet text, and associated coordinates from which the tweet was sent. 
 
-# I pulled parts of this from a twitter git code that I lost the link to, so not entirely original code here. Sorry can't identify it for now.
+# I pulled parts of this from a twitter git code here:
+# https://github.com/twitterdev/Twitter-API-v2-sample-code/blob/master/Full-Archive-Search/full-archive-search.py
 
 # Inputs: 
 # 	- Bearer Token
@@ -19,12 +20,12 @@ import requests, os, json, datetime, time
 import pandas as pd
 
 from datetime import timezone
+import calendar
 from pandas.io.json import json_normalize
 
 ## FUNCTIONAL SET UP ##
 
 ## functions ##
-
 
 # input the file where your bearer token is here
 def get_bearer_token():
@@ -40,15 +41,28 @@ def get_bearer_token():
 
 	return(headers)
 
+#imports the list of hateful keywords:
+# found here: https://github.com/mayelsherif/hate_speech_icwsm18/blob/master/hate_keywords.txt
+def import_hate_keywords():
+	with open("../d/hate_keywords.txt") as kw_raw:
+		keywords = kw_raw.readlines()
+		
+		for i in range(0,len(keywords)):
+			keywords[i] = keywords[i].replace('\n', '')
+
+	print("\nKeywords imported...")
+	return(keywords)
+
+
 #takes datetime object and turns it into a UTC string that twitter API will eat
 def convert_to_TwitTime(datetime_obj):
 	converted = datetime_obj.replace(tzinfo = timezone.utc).isoformat("T") #convert to UTC to rfc3339
 	return(converted)
 
 # takes query terms and turns them into dictionary
-def build_query(query, tweet_fields, place_fields, start_time, end_time,
-		max_resuts = 500, next_token = None):
-	print("\nBuilding query ... ")
+# by default approximates a query of all tweets
+def build_query(tweet_fields, place_fields, start_time, end_time,
+		max_resuts = 500, query = "lang:en the -the place_country:US has:geo", next_token = None):
 
 	start_time = convert_to_TwitTime(start_time)
 	end_time = convert_to_TwitTime(end_time)
@@ -65,30 +79,32 @@ def build_query(query, tweet_fields, place_fields, start_time, end_time,
 #taken from twitter example code
 def send_request(bearer_token, search_params, url = "https://api.twitter.com/2/tweets/search/all",
 		next_token = None):
-	print("\nSending request... ")
+
 	response = requests.request("GET", url, headers=bearer_token, params=search_params)
 
 	if response.status_code != 200:
 		raise Exception(response.status_code, response.text)
 
-	print("data received!")
 	return response.json()
 
 
-def get_data(query, bearer_token, search_params, url = "https://api.twitter.com/2/tweets/search/all"):
-	query_json = send_request(bearer_token = bearer_token, search_params= query)
+def get_data(auth_token, query, url = "https://api.twitter.com/2/tweets/search/all"):
+	query_json = send_request(bearer_token = auth_token, search_params= query)
 	tweet_df = clean(query_json)
 
+	#pagination
 	if "next_token" in list(tweet_df.columns.values):
 		next_page_id = tweet_df['next_token'].loc[0]
 	else:
 		next_page_id = None
 
+	time.sleep(18)
+
 	while(next_page_id!=None):
 
 		query["next_token"] = next_page_id
 
-		temp = send_request(bearer_token = bearer_token, search_params= query)
+		temp = send_request(bearer_token = auth_token, search_params= query)
 		temp = clean(temp)
 
 		tweet_df = tweet_df.append(temp, sort = True)
@@ -98,29 +114,49 @@ def get_data(query, bearer_token, search_params, url = "https://api.twitter.com/
 		else:
 			next_page_id = None
 
-		time.sleep(2)
+		time.sleep(10) #so i don't get banned from pulling
 
 	return(tweet_df)
 
 
 def clean(json):
-	print("Converting to DataFrame...")
-	cleaned_json = json_normalize(json, "data").assign(**json["meta"])
-	df = pd.DataFrame.from_dict(cleaned_json)
+	no_results = json_normalize(json)
 
-	return(df)
+	if no_results['meta.result_count'][0] == 0:
+		df = pd.DataFrame()
+		return(df)
+	else: 
+		cleaned_json = json_normalize(json, "data").assign(**json["meta"])
+		df = pd.DataFrame.from_dict(cleaned_json)
+		return(df)
 
 ## main method ##
 
 def main():
 	bearer_token = get_bearer_token()
-	query = build_query(query = "lang:en the -the place_country:US has:geo",
-		tweet_fields = "author_id,text,conversation_id,created_at,geo",
+	keywords = import_hate_keywords()
+
+	# for month in range(6,13):
+	tweets = pd.DataFrame()
+
+	# month_end_dt = calendar.monthrange(2014, month)[1]
+
+	# print("Getting data for {}.".format(month))
+
+	for word in reversed(keywords):
+		print("Querying for {}...".format(word))
+		query = build_query(tweet_fields = "author_id,text,conversation_id,created_at,geo",
 		place_fields = "country,country_code",
-		start_time = datetime.datetime(2011, 6, 1, 1, 2, 0),
-		end_time = datetime.datetime(2011, 6, 1, 1, 10, 10))
-	tweet_df = get_data(query, bearer_token = bearer_token, search_params= query)
-	tweet_df.to_csv("../d/tweet_df.csv")
+		start_time = datetime.datetime(2015, 1, 1, 0, 0, 0),
+		end_time = datetime.datetime(2015, 12, 31, 23, 59, 59),
+		query = "lang:en {} place_country:US has:geo".format(word))
+
+		tweet_df = get_data(auth_token = bearer_token, query = query)
+		tweet_df["keyword"] = word
+
+		tweets = tweets.append(tweet_df, sort = True)
+		tweets.to_csv("../d/tweet_df_2015_1_12_2.csv")
+	
 	print("DF saved to disk.")
 
 ## RUNNING RUNNING & RUNNING RUNNING & (https://youtu.be/IKqV7DB8Iwg) ##
