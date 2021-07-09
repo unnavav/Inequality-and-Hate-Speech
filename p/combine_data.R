c47755791cc8ed16458ec15346cd4764c916dd67
unnavav <unnava.v@gmail.com>
2021-07-09 11:13:43 -0700

    added an additional variable of interest

--- p/combine_data.R
+++ p/combine_data.R
@@ -54,8 +54,79 @@ merged = merged %>%
 #### Plotting some elementary stuff ----
 
 graph = merged %>%
-  mutate(chng_tweets = num_tweets/lag(num_tweets, 3)) %>%
+  mutate(chng_tweets = num_tweets/lag(num_tweets, 3),
+         chng_tweets = chng_tweets-1) %>%
   filter(year == 2017 | year == 2014)
 
+graph2 = merged %>% 
+  group_by(treat, year) %>%
+  summarise(num_tweets_all = sum(num_tweets, na.rm = T)) %>%
+  filter(year <= 2017 & year >= 2014)
+
+graph3 = merged %>% 
+  group_by(year, state, treat) %>%
+  summarise(num_tweets_state = sum(num_tweets, na.rm = T)) %>%
+  filter(year <= 2017 & year >= 2014)
+
+graph4 = graph3 %>%
+  ungroup() %>%
+  group_by(year, treat) %>%
+  summarise(num_tweets_all = sum(num_tweets_state, na.rm = T))
+
+graph5 = merged %>%
+  select(year, state, yoy_change, min_wage, treat) %>%
+  filter(year <= 2017 & year >= 2014) %>%
+  group_by(year, treat) %>%
+  summarise(min_wage = mean(min_wage, na.rm = F))
+
+png(filename="../v/inequality_corr_matrix.png", width=2400, height=1500, res = 300)
+
 ggplot(graph, aes(x = three_year_change, y = chng_tweets)) +
-  geom_point(aes(color = factor(treat)))
+  geom_point(aes(color = factor(treat))) + 
+  theme_minimal() +
+  scale_color_discrete(name = "Group", labels = c("Control", "Treatment")) +
+  labs(x="Change in Minimum Wage", y="Change in Number of Hate Speech Tweets (%)",
+       title = "Inequality and Hate Speech")
+
+dev.off()
+
+png(filename="../v/tweet_trends.png", width=2400, height=1500, res = 300)
+
+ggplot(graph2, aes(x = year, y = num_tweets_all)) +
+  geom_line(aes(color = factor(treat))) + 
+  theme_minimal() +
+  scale_color_discrete(name = "Group", labels = c("Control", "Treatment")) +
+  labs(x="Year", y="Number of Tweets Recorded",
+       title = "Change in Hate Speech Tweets Over Time")
+
+dev.off()
+
+
+ggplot(graph3, aes(x = year, y = num_tweets_state)) +
+  geom_point() + 
+  theme_minimal() +
+  scale_color_discrete(name = "Group", labels = c("Control", "Treatment")) +
+  labs(x="Year", y="Number of Tweets Recorded",
+       title = "Change in Hate Speech Tweets Over Time")
+
+png(filename="../v/tweet_trends_bar_split.png", width=2400, height=1500, res = 300)
+
+ggplot(graph4, aes(x = year, y = num_tweets_all, fill = factor(treat))) +
+  geom_bar(stat = 'identity') +
+  theme_minimal() +
+  scale_color_discrete(name = "Group", labels = c("Control", "Treatment")) +
+  labs(x="Year", y="Number of Tweets Recorded",
+       title = "Change in Hate Speech Tweets Over Time")
+
+dev.off()
+
+png(filename="../v/min_wage_trend.png", width=2400, height=1500, res = 300)
+
+ggplot(graph5, aes(x = year, y = min_wage)) +
+  geom_line(aes(color = factor(treat))) + 
+  theme_minimal() +
+  scale_color_discrete(name = "Group", labels = c("Control", "Treatment")) +
+  labs(x="Year", y="Simple Average of Minimum Wage",
+       title = "Change in Minimum Wage Over Time")
+
+dev.off()
